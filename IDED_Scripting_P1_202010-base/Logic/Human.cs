fafff7bbf4f9fd4125117c3895b734f8802a5942
Richman999 <migelyat@gmail.com>
2020-02-27 07:57:34 -0500

    Add files via upload
    Parcial.png

--- IDED_Scripting_P1_202010-base/Logic/Human.cs
+++ IDED_Scripting_P1_202010-base/Logic/Human.cs
@@ -7,12 +7,121 @@
         public Human(EUnitClass _unitClass, int _atk, int _def, int _spd, int _moveRange, float _potential)
             : base(_unitClass, _atk, _def, _spd, _moveRange)
         {
-            Potential = _potential;
+
+            if(_atk >= 255)
+            {
+                _atk = 255;
+            }
+            if(_def >= 255)
+            {
+                _def = 255;
+            }
+            if(_spd >= 255)
+            {
+                _spd = 255;
+            }
+            switch (_unitClass)
+            {
+                case EUnitClass.Villager:
+                    UnitClass = _unitClass;
+                    BaseAtk = 0;
+                    BaseDef = 0;
+                    BaseSpd = _spd;
+                    BaseAtkAdd = 0;
+                    BaseDefAdd = 0;
+                    BaseSpdAdd = 0;
+                    break;
+                case EUnitClass.Squire:
+                    UnitClass = _unitClass;
+                    AtkRange = 1;
+                    BaseAtk = _atk;
+                    BaseDef = _def;
+                    BaseSpd = _spd;
+                    BaseAtkAdd = 2;
+                    BaseDefAdd = 1;
+                    BaseSpdAdd = 0;
+                    break;
+                case EUnitClass.Soldier:
+                    UnitClass = _unitClass;
+                    AtkRange = 1;
+                    BaseAtk = _atk;
+                    BaseDef = _def;
+                    BaseSpd = _spd;
+                    BaseAtkAdd = 3;
+                    BaseDefAdd = 2;
+                    BaseSpdAdd = 1;
+                    break;
+                case EUnitClass.Ranger:
+                    UnitClass = _unitClass;
+                    AtkRange = 3;
+                    BaseAtk = _atk;
+                    BaseDef = _def;
+                    BaseSpd = _spd;
+                    BaseAtkAdd = 1;
+                    BaseDefAdd = 0;
+                    BaseSpdAdd = 3;
+                    break;
+                case EUnitClass.Mage:
+                    UnitClass = _unitClass;
+                    AtkRange = 3;
+                    BaseAtk = _atk;
+                    BaseDef = _def;
+                    BaseSpd = _spd;
+                    BaseAtkAdd = 3;
+                    BaseDefAdd = 1;
+                    BaseSpdAdd = -1;
+                    break;
+                case EUnitClass.Imp:
+                    _unitClass = EUnitClass.Villager;
+                    break;
+                case EUnitClass.Orc:
+                    _unitClass = EUnitClass.Villager;
+                    break;
+                case EUnitClass.Dragon:
+                    _unitClass = EUnitClass.Villager;
+                    break;
+                default:
+                    _unitClass = EUnitClass.Villager;
+                    break;
+            }
+            if(_potential >= 10)
+            {
+                _potential = 10;
+            }else
+            {
+                Potential = _potential;
+            }
+
+            Attack = CalculoPorcentualEstadistico(BaseAtk, BaseAtkAdd);
+            Defense = CalculoPorcentualEstadistico(BaseDef, BaseDefAdd);
+            Speed = CalculoPorcentualEstadistico(BaseSpd, BaseSpdAdd);
+
         }
 
         public virtual bool ChangeClass(EUnitClass newClass)
         {
+            if(UnitClass == EUnitClass.Squire && newClass == EUnitClass.Soldier)
+            {
+                UnitClass = newClass;
+            }
+            if (UnitClass == EUnitClass.Ranger && newClass == EUnitClass.Mage)
+            {
+                UnitClass = newClass;
+            }
             return false;
+        }
+        public override bool Interact(Prop prop)
+        {
+            if (UnitClass == EUnitClass.Villager)
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
--- IDED_Scripting_P1_202010-base/Logic/Unit.cs
+++ IDED_Scripting_P1_202010-base/Logic/Unit.cs
@@ -13,9 +13,9 @@
         public float BaseDefAdd { get; protected set; }
         public float BaseSpdAdd { get; protected set; }
 
-        public float Attack { get; }
-        public float Defense { get; }
-        public float Speed { get; }
+        public float Attack { get; protected set; }
+        public float Defense { get; protected set; }
+        public float Speed { get; protected set; }
 
         protected Position CurrentPosition;
 
@@ -28,6 +28,30 @@
             BaseDef = _def;
             BaseSpd = _spd;
             MoveRange = _moveRange;
+            switch (_unitClass)
+            {
+                case EUnitClass.Imp:
+                    AtkRange = 1;
+                    BaseAtkAdd = 1;
+                    BaseDefAdd = 0;
+                    BaseSpdAdd = 0;
+                    break;
+                case EUnitClass.Orc:
+                    AtkRange = 1;
+                    BaseAtkAdd = 4;
+                    BaseDefAdd = 2;
+                    BaseSpdAdd = -2;
+                    break;
+                case EUnitClass.Dragon:
+                    AtkRange = 5;
+                    BaseAtkAdd = 5;
+                    BaseDefAdd = 3;
+                    BaseSpdAdd = 2;
+                    break;
+            }
+            Attack = CalculoPorcentualEstadistico(BaseAtk, BaseAtkAdd);
+            Defense = CalculoPorcentualEstadistico(BaseDef, BaseDefAdd);
+            Speed = CalculoPorcentualEstadistico(BaseSpd, BaseSpdAdd);
         }
 
         public virtual bool Interact(Unit otherUnit)
@@ -38,5 +62,45 @@
         public virtual bool Interact(Prop prop) => false;
 
         public bool Move(Position targetPosition) => false;
+
+        public float CalculoPorcentualEstadistico(int bas, float porcent)
+        {
+            float a;
+            float sumPor;
+            sumPor = ((bas / 100) * porcent);
+            a = bas + sumPor;
+            if(a <= 255)
+            {
+                return a;
+            }if(a >= 255)
+            {
+                return 255;
+            }else
+            {
+                return 0;
+            }
+
+        }
+        public float CalculoPorcentualEstadistico(int bas, float porcent, float potencial)
+        {
+            float a;
+            float sumPor;
+            float sumPot;
+            sumPot = ((bas / 100) * potencial);
+            sumPor = ((bas / 100) * porcent);
+            a = bas + sumPor + sumPot;
+            if (a <= 255)
+            {
+                return a;
+            }
+            if (a >= 255)
+            {
+                return 255;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
